7e72c05d397c0fabfba66edaa12608aeb8750362
Isaac Molina <izaac.everaldo@gmail.com>
2022-03-13 20:56:42 -0600

    inicialización de repo

---
+++ public/assets/Plugins/AutoPause.js
@@ -0,0 +1,41 @@
+class AutoPause {
+    constructor() {
+        this.threshold = 0.25;
+        this.player = undefined;
+        this.handlerIntersection = this.handlerIntersection.bind(this);
+        this.handlerVisibilityChange = this.handlerVisibilityChange.bind(this);
+    }
+
+}
+
+AutoPause.prototype.run = function (player) {
+    this.player = player.media;
+    const observer = new IntersectionObserver(this.handlerIntersection, {
+        threshold: this.threshold
+    });
+    observer.observe(player.media);
+    document.addEventListener('visibilitychange', this.handlerVisibilityChange);
+}
+
+AutoPause.prototype.handlerIntersection = function (entries) {
+    const entry = entries[0];
+    console.log(entry);
+
+    const isVisible = entry.intersectionRatio >= this.threshold;
+    if (isVisible) {
+        this.player.play();
+    } else {
+        this.player.pause();
+    }
+}
+
+AutoPause.prototype.handlerVisibilityChange = function () {
+    const isVisible = document.visibilityState === "visible";
+    if (isVisible) {
+        this.player.play();
+    } else {
+        this.player.pause();
+    }
+};
+
+export default AutoPause;
